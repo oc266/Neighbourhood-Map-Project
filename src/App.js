5b58c0333ee0c1720412f9c656177d748f9260b8
Oliver Critchfield <oliver.critchfield@gmail.com>
2018-08-09 20:39:29 +0100

    Initial commit, with map and sidebar/ header layout

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { GoogleApiWrapper } from 'google-maps-react';
+import MapContainer from './MapContainer.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <a className="places-hamburger">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
+            </svg>
+          </a>
+          <h1 className="app-title">Neighborhood Map</h1>
+        </header>
+        <section className="places-list">
+          <p>Hello</p>
+        </section>
+        <MapContainer
+          google={this.props.google}
+        />
+      </div>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyA4C2NSjx4sMd73sbPuafBS6mjCnr_sYVE'
+})(App);
---
+++ src/MapContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { GoogleApiWrapper, InfoWindow, Marker, Map } from 'google-maps-react';
+import './App.css';
+
+class MapContainer extends Component {
+
+  render() {
+    return (
+      <div className='map-container'>
+        <Map
+          google={this.props.google}
+          zoom={13}
+          initialCenter={{
+            lat: 55.6805,
+            lng: 12.5860
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+export default MapContainer
